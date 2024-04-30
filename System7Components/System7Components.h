e15cbbb156dbbe97a9bea58c9c09eb4636cd8349
Jan Huber <huber@ubique.ch>
2024-04-30 21:32:59 +0200

    Initial Commit

---
+++ System7Components/System7Components.h
@@ -0,0 +1,18 @@
+//
+//  System7Components.h
+//  System7Components
+//
+//  Created by Jan Huber on 30.04.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for System7Components.
+FOUNDATION_EXPORT double System7ComponentsVersionNumber;
+
+//! Project version string for System7Components.
+FOUNDATION_EXPORT const unsigned char System7ComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <System7Components/PublicHeader.h>
+
+
